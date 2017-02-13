b9f5a0f9a92ee376c2d773223c9429ea2394b5c3
exit <quit>
2017-02-13 13:48:16 +0300

    InsertionSortPart2 done

--- src/hackerrank/algorithms/sorting/InsertionSortPart2.java
+++ src/hackerrank/algorithms/sorting/InsertionSortPart2.java
@@ -1,9 +1,53 @@
 package hackerrank.algorithms.sorting;
 
+import java.util.Scanner;
+
 public class InsertionSortPart2 {
 
-	public static void main(String[] args) {
-
-	}
+	public static void insertionSortPart2(int[] ar) {
+        // Fill up this function 
+        if (ar==null || ar.length == 1)
+            return;
+        
+        int i = -1;
+        int j = 1;
+        
+        for (; j < ar.length; j++){
+        	
+            if (ar[j] < ar[j - 1]){
+                int temp = ar[j];
+                i = j - 1;
+                while (i >= 0 && ar[i] > temp){
+                    ar[i + 1] = ar[i];
+                    ar[i] = temp;
+                    i--;
+//                    printArray(ar);
+                }
+                
+                printArray(ar);
+            }else
+            	printArray(ar);
+        }
+    }
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertionSortPart2(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
 
 }
